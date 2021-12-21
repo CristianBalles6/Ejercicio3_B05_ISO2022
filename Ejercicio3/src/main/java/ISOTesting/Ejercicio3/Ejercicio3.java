a0785577b57beb4a13ae6f1897e6e8adbd408d93
CristianBalles6 <91469560+CristianBalles6@users.noreply.github.com>
2021-12-21 11:41:39 +0100

    Add files via upload

---
+++ Ejercicio3/src/main/java/ISOTesting/Ejercicio3/Ejercicio3.java
@@ -0,0 +1,114 @@
+package ISOTesting.Ejercicio3;
+
+import java.io.IOException;
+import java.math.*;
+import java.util.Scanner;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce los datos de completitud");
+		int completitud = sc.nextInt();
+		System.out.println("Introduce los datos de correcion");
+		int correcion = sc.nextInt();
+		System.out.println("Introduce los datos de pertinencia");
+		int pertinencia = sc.nextInt();
+
+		completitud = algo(completitud, 0, 1, 2, 2, 3, 4);
+		correcion = algo(correcion, 0, 1, 1, 2, 3, 5);
+		pertinencia = algo(pertinencia, 0, 2, 2, 3, 4, 5);
+
+		int adecuacion = Math.min(completitud, Math.min(correcion, pertinencia));
+		System.out.println(adecuacion);
+		
+		System.out.println("Introduce los datos de modularidad");
+		int modularidad = sc.nextInt();
+		System.out.println("Introduce los datos de reusabilidad");
+		int reusabilidad = sc.nextInt();
+		System.out.println("Introduce los datos de analizabilidad");
+		int analizabilidad = sc.nextInt();
+		System.out.println("Introduce los datos de modificado");
+		int modificado = sc.nextInt();
+		System.out.println("Introduce los datos de probado");
+		int probado = sc.nextInt();
+
+		modularidad = algo(modularidad, 0, 1, 2, 2, 3, 4);
+		reusabilidad = algo(reusabilidad, 0, 1, 2, 2, 3, 5);
+		analizabilidad = algo(analizabilidad, 0, 0, 1, 2, 3, 5);
+		modificado = algo(modificado, 0, 1, 2, 3, 4, 5);
+		probado = algo(probado, 0, 1, 1, 2, 4, 4);
+
+		int aux1 = Math.min(modularidad, reusabilidad);
+		int aux2 = Math.min(modificado, probado);
+		int mantenibilidad = Math.min(aux1, Math.min(aux2, analizabilidad));
+		System.out.println(mantenibilidad);
+		int calidad = calcular(adecuacion, mantenibilidad);
+System.out.println(calidad);
+		if (calidad >= 3)
+			System.out.println("Toma tu certificado");
+		else
+			System.out.println("Tienes que ir a extraordinaria");
+	}
+
+	public static int calcular(int ad, int man) {
+		int calidad = 0;
+		try {
+			if (ad == 0 || man == 0)
+				throw new Exception("Error, calidad muy baja");
+			else {
+				if (ad == 1)
+					calidad = 1;
+				else if (ad == 2 && man == 1)
+					calidad = 1;
+				else if (ad == 2 && man != 1)
+					calidad = 2;
+				else if (ad == 3 && (man == 1 || man == 2))
+					calidad = 2;
+				else if (ad == 3 && man > 2)
+					calidad = 3;
+				else if (ad == 4 && man == 5)
+					calidad = 4;
+				else if (ad == 4 && man != 5)
+					calidad = 3;
+				else if (ad == 5 && (man == 1 || man == 2))
+					calidad = 3;
+				else if (ad == 5 && man == 5)
+					calidad = 5;
+				else
+					calidad = 4;
+
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return calidad;
+	}
+
+	public static int algo(int valor, int v1, int v2, int v3, int v4, int v5, int v6) {
+		try {
+			if (valor < 0 || valor > 100)
+				throw new Exception("Valor introducido erroneo");
+				
+			if (valor < 10) {
+				valor = v1;
+			} else if (valor >= 10 && valor < 35) {
+				valor = v2;
+			} else if (valor >= 35 && valor < 50) {
+				valor = v3;
+			} else if (valor >= 50 && valor < 70) {
+				valor = v4;
+			} else if (valor >= 70 && valor < 90) {
+				valor = v5;
+			} else {
+				valor = v6;
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.notify();
+		}
+	
+		return valor;
+	}
+
+}
---
+++ Ejercicio3/src/test/java/ISOTesting/Ejercicio3/Ejercicio3Test.java
@@ -0,0 +1,151 @@
+package ISOTesting.Ejercicio3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Ejercicio3Test {
+	Ejercicio3 prueba = new Ejercicio3();
+
+	@Test
+	public void testCalcular1() {
+		int calidad1 = prueba.calcular(1, 3);
+		assertEquals(1, calidad1);
+	}
+
+	@Test
+	public void testCalcular2() {
+		int calidad2 = prueba.calcular(0, 2);
+		assertEquals(1, calidad2);
+	}
+
+	@Test
+	public void testCalcular3() {
+		int calidad3 = prueba.calcular(2, 3);
+		assertEquals(2, calidad3);
+	}
+
+	@Test
+	public void testCalcular4() {
+		int calidad4 = prueba.calcular(2, 1);
+		assertEquals(1, calidad4);
+	}
+
+	@Test
+	public void testCalcular5() {
+		int calidad5 = prueba.calcular(3, 3);
+		assertEquals(3, calidad5);
+	}
+
+	@Test
+	public void testCalcular6() {
+		int calidad6 = prueba.calcular(3, 2);
+		assertEquals(2, calidad6);
+	}
+
+	@Test
+	public void testCalcular7() {
+		int calidad7 = prueba.calcular(3, 1);
+		assertEquals(2, calidad7);
+	}
+
+	@Test
+	public void testCalcular8() {
+		int calidad8 = prueba.calcular(4, 3);
+		assertEquals(3, calidad8);
+	}
+
+	@Test
+	public void testCalcular9() {
+		int calidad9 = prueba.calcular(4, 5);
+		assertEquals(4, calidad9);
+	}
+
+	@Test
+	public void testCalcular10() {
+		int calidad10 = prueba.calcular(5, 3);
+		assertEquals(4, calidad10);
+	}
+
+	@Test
+	public void testCalcular11() {
+		int calidad11 = prueba.calcular(5, 2);
+		assertEquals(3, calidad11);
+	}
+
+	@Test
+	public void testCalcular12() {
+		int calidad12 = prueba.calcular(5, 1);
+		assertEquals(3, calidad12);
+	}
+
+	@Test
+	public void testCalcular13() {
+		int calidad13 = prueba.calcular(5, 5);
+		assertEquals(5, calidad13);
+	}
+
+	@Test
+	public void testAlgo1() {
+
+		int completitud = prueba.algo(50, 0, 1, 2, 2, 3, 4);
+		assertEquals(2, completitud);
+	}
+
+	@Test
+	public void testAlgo2() {
+
+		try {
+			int correcion = prueba.algo(300, 0, 1, 1, 2, 4, 4);
+		} catch (Exception e) {
+			Assert.fail("Valor introducido erroneo");
+		}
+	}
+
+	@Test
+	public void testAlgo3() {
+
+		int pertinencia = prueba.algo(0, 0, 2, 2, 3, 4, 5);
+		assertEquals(0, pertinencia);
+	}
+
+	@Test
+	public void testAlgo4() {
+
+		int modularidad = prueba.algo(30, 0, 1, 2, 2, 3, 4);
+		assertEquals(1, modularidad);
+	}
+
+	@Test
+	public void testAlgo5() {
+
+		int reusabilidad = prueba.algo(40, 0, 1, 2, 2, 3, 5);
+		assertEquals(2, reusabilidad);
+	}
+
+	@Test
+	public void testAlgo6() {
+
+		int analizabilidad = prueba.algo(80, 0, 0, 1, 2, 3, 5);
+		assertEquals(3, analizabilidad);
+	}
+
+	@Test
+	public void testAlgo7() {
+
+		int modificado = prueba.algo(95, 0, 1, 2, 3, 4, 5);
+		assertEquals(5, modificado);
+	}
+
+	@Test
+	public void testAlgo8() {
+		try {
+			int probado = prueba.algo(-5, 0, 1, 1, 2, 4, 4);
+		} catch (Exception e) {
+			Assert.fail("Valor introducido erroneo");
+		}
+
+	}
+
+}
